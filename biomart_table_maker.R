84db55a7f4e35756e6947fb2983de81ddbbd6e10
Travis <travis.m.blimkie@gmail.com>
2019-07-03 14:56:12 -0700

    Add files via upload
    Simple script to make biomart table for ID conversions.

---
+++ biomart_table_maker.R
@@ -0,0 +1,25 @@
+
+cat("Loading libraries...\n")
+
+library(biomaRt)
+library(tidyverse)
+
+
+todays_date <- str_replace_all(Sys.Date(), pattern = "-", replacement = "")
+
+
+cat("\nCreate biomart table...\n")
+
+biomart_table <- getBM(
+  attributes = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id"),
+  mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+)
+biomart_table[biomart_table == ""] <- NA
+
+
+cat("\nSave output...\n")
+
+saveRDS(biomart_table, file = paste0("~/biomart_table_", todays_date, ".rds"))
+
+
+cat("\nDONE!\n")
